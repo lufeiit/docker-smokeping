f47704097e81ac07364c7fe7677c52b2e7782431
lufeiit <32061921+lufeiit@users.noreply.github.com>
2019-12-05 01:34:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/with-contenv bash
 
-if [ $SECRET $URL $DIR $NAME ]; then
-    echo $SECRET >/etc/smokeping/smokeping_secrets \
-    && exec s6-setuidgid abc  /usr/bin/smokeping --master-url=$URL/smokeping.cgi --shared-secret=/etc/smokeping/smokeping_secrets --cache-dir=$DIR --slave-name=$NAME --nodaemon 
+if [ $SECRET ]; then
+    echo $SECRET >/etc/smokeping/smokeping_secrets
 fi
 
 chown -R 600 /etc/smokeping/smokeping_secret
